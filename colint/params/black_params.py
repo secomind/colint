2932492530b4262ac932a8e1062e141fc73835da
Davide Colì <davide.coli@secomind.com>
2024-09-18 17:38:26 +0200

    BUGs: Fixing usage of code-format
    Fixes two bugs:
    - TargetVersion was not handled correctly when parsing toml file.
    - for some reason the "NothingChanged" raised by black reports
      breaks the format_functions
    
    Fixes consists in:
    - added an explicit mapper from string to black's TargetVersion.
    - created a `__get_formatted_text` function which catches the
      "NothingChanged" warning.
    
    Signed-off-by: Davide Colì <davide.coli@secomind.com>

--- colint/params/black_params.py
+++ colint/params/black_params.py
@@ -1,5 +1,21 @@
 from dataclasses import dataclass, field
 
+from black.mode import TargetVersion
+
+_str_to_target_version = {
+    "py3": TargetVersion.PY33,
+    "py4": TargetVersion.PY34,
+    "py5": TargetVersion.PY35,
+    "py6": TargetVersion.PY36,
+    "py7": TargetVersion.PY37,
+    "py8": TargetVersion.PY38,
+    "py9": TargetVersion.PY39,
+    "py10": TargetVersion.PY310,
+    "py11": TargetVersion.PY311,
+    "py12": TargetVersion.PY312,
+    "py13": TargetVersion.PY313,
+}
+
 
 @dataclass
 class BlackParams:
@@ -13,10 +29,18 @@ class BlackParams:
         unstable (bool): Whether to enable unstable features.
     """
 
-    target_version: list[str] = field(default_factory=lambda: ["py10"])
+    target_version: set[TargetVersion] = field(
+        default_factory=lambda: {TargetVersion.PY310}
+    )
     line_length: int = 88
     preview: bool = False
     unstable: bool = False
+
+    @staticmethod
+    def convert_to_target_version(key: str) -> TargetVersion:
+        if key not in _str_to_target_version:
+            raise KeyError(f"Unsupported target version: {key}.")
+        return _str_to_target_version[key]
 
     @staticmethod
     def from_dict(obj: dict) -> "BlackParams":
@@ -32,6 +56,7 @@ class BlackParams:
         versions = obj.get("target-version", ["py10"])
         if isinstance(versions, str):
             versions = [versions]
+        versions = set(_str_to_target_version[k] for k in versions)
 
         try:
             line_length = int(obj.get("line_length", 88))
@@ -48,4 +73,9 @@ class BlackParams:
         except:
             unstable = False
 
-        return BlackParams(target_version=versions, line_length=line_length, preview=preview, unstable=unstable)
+        return BlackParams(
+            target_version=versions,
+            line_length=line_length,
+            preview=preview,
+            unstable=unstable,
+        )
